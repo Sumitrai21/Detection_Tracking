deaf57dc5540df63eacac70985a70eb29f2535a3
Sumitrai21 <sumit18101@iiitnr.edu.in>
2021-05-31 07:02:14 +0530

    Adding the tracker class

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ lk_params = dict(winSize=(10,10),
 def main():
     image = cv2.imread('image.jpg')
     faces = face_detector(image)
-    print(len(faces))
-    print(type(faces))
+    #print(len(faces))
+    #print(type(faces))
     image = draw_box(image,faces)
 
     cv2.imshow('frame',image)   
@@ -32,8 +32,24 @@ def main():
 
     cv2.destroyAllWindows
 
+def test():
+    cap = cv2.VideoCapture(0)
+    #slow as we have not used numpy yet
+    while(cap.isOpened()):
+        _,frame = cap.read()
+        faces = face_detector(frame)
+        frame = draw_box(frame,faces)
+
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+
+    cv2.destroyAllWindows()
+    cap.release()
+
 
 if __name__ == '__main__':
-    main()
+    test()
 
 
--- tracker.py
+++ tracker.py
@@ -1,7 +1,31 @@
 import cv2
 import numpy
 
-def lucas_kanade(old_gray,gray_frame,old_points,**lk_params):
+
+
+
+class LucasKanade:
+    def __init__(self,old_gray,new_gray,old_points):
+        self.old_gray = old_gray
+        self.new_gray = new_gray
+        self.old_points = old_points
+        #**self.lk_params = **lk_params
+
+
+    def forward(self,**lk_parameters):
+        if self.new_gray == None:
+            pass
+
+        new_points, status,error = cv2.calcOpticalFlowPyrLK(self.old_gray,self.new_gray,self.old_points,None,self.lk_params)
+
+        return new_points,status,error
+
+
+
+def lucas_kanade(old_gray,gray_frame=None,old_points = None,**lk_params):
     # This function uses the lucas kanade optical flow,i.e the tracker
+    if gray_frame == None:
+        pass
+    
     new_point,status,error = cv2.calcOpticalFlowPyrLK(old_gray,gray_frame,old_points,None,**lk_params)
     return new_point,status,error
